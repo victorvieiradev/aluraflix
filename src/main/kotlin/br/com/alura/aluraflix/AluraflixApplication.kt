389681ff80f9ea80e7747ae254139eb418fac12d
Victor Vieira <contatovictorvieira@outlook.com>
2023-04-29 20:53:31 -0300

    Cria estrutura inicial do projeto

---
+++ src/main/kotlin/br/com/alura/aluraflix/AluraflixApplication.kt
@@ -0,0 +1,11 @@
+package br.com.alura.aluraflix
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AluraflixApplication
+
+fun main(args: Array<String>) {
+	runApplication<AluraflixApplication>(*args)
+}
