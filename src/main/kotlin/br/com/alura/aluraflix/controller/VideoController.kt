ef1e93044b4438e14196b5cb1c8c1e53b9862589
Victor Vieira <contatovictorvieira@outlook.com>
2023-04-30 16:17:36 -0300

    Implementa método de atualizar vídeo

--- src/main/kotlin/br/com/alura/aluraflix/controller/VideoController.kt
+++ src/main/kotlin/br/com/alura/aluraflix/controller/VideoController.kt
@@ -1,17 +1,13 @@
 package br.com.alura.aluraflix.controller
 
+import br.com.alura.aluraflix.dto.AtualizaVideoForm
 import br.com.alura.aluraflix.dto.VideoFormDto
 import br.com.alura.aluraflix.dto.VideoViewDto
 import br.com.alura.aluraflix.service.VideoService
 import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/videos")
@@ -36,5 +32,14 @@ return ResponseEntity.ok(videoService.buscarPorId(id))
     fun excluir(@PathVariable id: Long): ResponseEntity<String> {
         videoService.excluir(id)
         return ResponseEntity.ok().body("Excluído.")
+    }
+    @PutMapping
+    @ResponseStatus(HttpStatus.OK)
+    fun atualizar(
+        @RequestBody
+        @Valid
+        atualizaVideoForm: AtualizaVideoForm
+    ): VideoViewDto {
+        return videoService.atualiza(atualizaVideoForm)
     }
 }
--- src/main/kotlin/br/com/alura/aluraflix/service/VideoService.kt
+++ src/main/kotlin/br/com/alura/aluraflix/service/VideoService.kt
@@ -1,10 +1,12 @@
 package br.com.alura.aluraflix.service
 
+import br.com.alura.aluraflix.dto.AtualizaVideoForm
 import br.com.alura.aluraflix.dto.VideoFormDto
 import br.com.alura.aluraflix.dto.VideoViewDto
 import br.com.alura.aluraflix.exceptions.NotFoundException
 import br.com.alura.aluraflix.mapper.VideoFormDtoMapper
 import br.com.alura.aluraflix.mapper.VideoViewMapper
+import br.com.alura.aluraflix.model.VideoModel
 import br.com.alura.aluraflix.repository.VideoRepository
 import org.springframework.stereotype.Service
 
@@ -21,13 +23,28 @@ class VideoService(
     }
 
     fun buscarPorId(id: Long): VideoViewDto {
-        val video = videoRepository.findById(id).orElseThrow{
+        val video = videoRepository.findById(id).orElseThrow {
             NotFoundException(mensagem = "O vídeo $id não foi encontrado.")
         }
         return videoViewMapper.map(video)
     }
-    fun excluir(id: Long){
+
+    fun excluir(id: Long) {
         val video = buscarPorId(id)
         videoRepository.deleteById(video.id!!)
+    }
+
+    fun atualiza(atualizaVideoForm: AtualizaVideoForm): VideoViewDto {
+        val video = buscarPorId(atualizaVideoForm.id)
+        return videoViewMapper.map(
+            videoRepository.save(
+                VideoModel(
+                    id = atualizaVideoForm.id,
+                    titulo = atualizaVideoForm.titulo ?: video.titulo,
+                    descricao = atualizaVideoForm.descricao ?: video.descricao,
+                    url = atualizaVideoForm.url ?: video.url,
+                )
+            )
+        )
     }
 }
